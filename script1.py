f02e490f5f88efd970f34eefc845f40eff14a0de
lmrs-06 <94164280+lmrs-06@users.noreply.github.com>
2022-11-30 20:21:09 +0100

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,49 @@
+#Sistema importado para la fecha y hora
+import time
+import os
+os.system("clear")
+#Prueba de usuario
+print("Hecho por Luis MIguel Rodriguez Suarez")
+print( )
+#Variables
+fecha = time.strftime("%x")
+hora = time.strftime("%X")
+t1=0
+temperatura= []
+#Introduccion
+print("A continuacion, vamos a proceder con el registro de temperatura de Doñana")
+print( )
+print("¡Aviso!")
+print("A partir de los 100ºC, se detendra el escaneo")
+print( )
+#Principal
+while True:	
+	temp = int and float(input("Indica la temperatura de estos dias: "))
+	if temp < 100:
+		temperatura.append(temp)
+		t1+=1
+	if temp >= 100:
+		print("Registro terminado") 
+		print("Generando informe:")
+		print( )
+		break;	
+#Variables secundarias
+t2=max(temperatura)
+t3=min(temperatura)
+t4=sum(temperatura)/t1
+#Tupla con redireccion a fichero .txt
+tupla=(fecha,hora,t1,t2,t3,t4)
+genera=list(tupla)
+file=open("inf-temperatura.txt","a")
+file.write('%s' % genera +'\n')
+#Informe
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("Informe de temperaturas del Parque Nacional de Doñana")
+print("Fecha:",fecha)
+print("Hora:",hora)
+print( )
+print("Numero de muestras realizadas:",t1)
+print("Temperatura Maxima Registrada:",t2)
+print("Temperatura Minima Registrada",t3)
+print("Tempertura media general:",round(t4,1))
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
---
+++ script2.py
@@ -0,0 +1,42 @@
+
+#Sistema importado para la fecha y hora
+import random
+import os
+import time
+os.system("clear") #comando que llama al sistema y ejecuta el comando clear
+#Prueba de usuario
+print("Hecho por Luis MIguel Rodriguez Suarez")
+print( )
+#Variables
+fecha = time.strftime("%x") #variable de fecha
+hora = time.strftime("%X") #variable de hora 
+newvariante=str("ccucggcgggca") #seleccionamos la cadena de la nueva variante
+codigo=input("Introduzca el codigo del paciente: ") #pedimos un codigo de paciente
+numero=10000000 #longitud de cadena de ADN, cuanto mas larga, mas posibilidad de distintas respuestas
+letras=["c","u","g","a"] #seleccionamos los componentes de la nueva variante
+nletras=len(letras) #cuenta el nº de letras de la nueva variante
+aleatorio=[]
+#Principal
+for i in range(0, numero, 1):
+    numaleatorio = random.randint(0, nletras - 1) #generamos la cadena aleatoria
+    aleatorio.append(letras[numaleatorio]) #el aleatorio se añade a la cadena de adn
+generado="".join(aleatorio)
+#Resultado
+resultado = ""
+if newvariante in generado: #se comprueba si el paciente es positivo o no mediante buscar la nueva variante con el adn que hemos generado
+    resultado = "Positivo: Sí se encuentra restos de la variante COVID."
+else:
+    resultado = "Negativo: No se encuentra restos de la variante COVID."
+#Tupla con redireccion a fichero .txt
+tupla=(fecha,hora,codigo,resultado)
+genera=list(tupla)
+file=open("inf-covid.txt","a")
+file.write('%s' % genera +'\n')
+#Informe COVID
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("Informe de Virus COVID:")
+print("Fecha:",fecha)
+print("Hora:",hora)
+print("Código del paciente:",codigo)
+print("Resultado:",resultado)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
